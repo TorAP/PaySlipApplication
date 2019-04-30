0100a16b9768080be5e00417ac6208f2979237c5
malteerasmussen <malteerasmussen@gmail.com>
2019-04-30 11:44:30 +0200

    Created methods for CaseMenu class

--- app/src/main/java/com/example/pay_slip_application/CaseMenu.java
+++ app/src/main/java/com/example/pay_slip_application/CaseMenu.java
@@ -2,7 +2,10 @@ package com.example.pay_slip_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.HorizontalScrollView;
 
 public class CaseMenu extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class CaseMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_case_menu);
+    }
+
+    public void onClickCase1(View view) {
+        //   Intent intent = new Intent(this, SupplementCase.class);
+        //   startActivity(intent);
+    }
+
+    public void onClickBack(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
