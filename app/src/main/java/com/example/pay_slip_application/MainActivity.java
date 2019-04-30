042c13f7b268778a4edba90c06602536fd9b0a49
Tor Petersen <tor.arnth.petersen@gmail.com>
2019-04-30 12:24:13 +0200

    changes

--- app/src/main/java/com/example/pay_slip_application/MainActivity.java
+++ app/src/main/java/com/example/pay_slip_application/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pay_slip_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+
+
+
+
 }
