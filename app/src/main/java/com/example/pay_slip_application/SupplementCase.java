2d5cc7fbb8f1fb08406ec7582eda63d2ad5aa56b
malteerasmussen <malteerasmussen@gmail.com>
2019-05-02 10:15:31 +0200

    Worked on drag and drop

--- app/src/main/java/com/example/pay_slip_application/SupplementCase.java
+++ app/src/main/java/com/example/pay_slip_application/SupplementCase.java
@@ -3,15 +3,18 @@ package com.example.pay_slip_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ClipData;
+import android.content.ClipDescription;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SupplementCase extends AppCompatActivity {
 
-    TextView textView, textView1, textView2, textView3;
-
+    TextView dragger1, dragger2, dragger3, dropper1, dropper2, dropper3;
+    private static String DRAGGER_TAG = "Drag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +22,31 @@ public class SupplementCase extends AppCompatActivity {
         setContentView(R.layout.activity_supplement_case);
 
 
-        textView = (TextView) findViewById(R.id.textView);
-        textView1 = (TextView) findViewById(R.id.textView1);
-        textView2 = (TextView) findViewById(R.id.textView2);
-        textView3 = (TextView) findViewById(R.id.textView3);
+        dragger1 = (TextView) findViewById(R.id.dragName);
+        dragger1.setTag(DRAGGER_TAG);
+        dragger2 = (TextView) findViewById(R.id.dragCpr);
+        dragger2.setTag(DRAGGER_TAG);
+        dragger3 = (TextView) findViewById(R.id.dragMonth);
+        dragger3.setTag(DRAGGER_TAG);
+
+        dropper1 = (TextView) findViewById(R.id.dropName);
+        dropper2 = (TextView) findViewById(R.id.dropCpr);
+        dropper3 = (TextView) findViewById(R.id.dropMonth);
 
 
-        textView.setOnLongClickListener(longClickListener);
-        textView1.setOnLongClickListener(longClickListener);
-        textView2.setOnLongClickListener(longClickListener);
-        textView3.setOnLongClickListener(longClickListener);
+        dragger1.setOnLongClickListener(longClickListener);
+        dragger2.setOnLongClickListener(longClickListener);
+        dragger3.setOnLongClickListener(longClickListener);
+        dropper1.setOnLongClickListener(longClickListener);
+        dropper2.setOnLongClickListener(longClickListener);
+        dropper3.setOnLongClickListener(longClickListener);
 
-        textView3.setOnDragListener(dragListener);
+        dragger1.setOnDragListener(dragListener);
+        dragger2.setOnDragListener(dragListener);
+        dragger3.setOnDragListener(dragListener);
+        dropper1.setOnDragListener(dragListener);
+        dropper2.setOnDragListener(dragListener);
+        dropper3.setOnDragListener(dragListener);
 
 
     }
@@ -41,19 +57,20 @@ public class SupplementCase extends AppCompatActivity {
         @Override
         public boolean onLongClick(View v) {
 
-            // Create a new ClipData holding data of the type ClipDescription#MIMETYPE_TEXT_PLAIN.
-            ClipData data = ClipData.newPlainText("", "");
+            if (((TextView) v).getText() != "") {
+                ClipData.Item item = new ClipData.Item(((TextView) v).getText().toString());
+                String[] mimeTypes = {ClipDescription.MIMETYPE_TEXT_PLAIN};
 
 
-            //Constructs a shadow image builder based on the View v .
-            View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
+                ClipData data = new ClipData(((TextView) v).getText(), mimeTypes, item);
 
+                View.DragShadowBuilder myShadow = new View.DragShadowBuilder(v);
 
-            //The startDrag-method 
-            v.startDrag(data, myShadowBuilder, v, 0);
+                v.startDragAndDrop(data, myShadow, v, 0);
 
-            return true;
-
+                //v.setVisibility(View.INVISIBLE);
+                return true;
+            } else return false;
         }
 
 
@@ -64,41 +81,67 @@ public class SupplementCase extends AppCompatActivity {
         @Override
         public boolean onDrag(View v, DragEvent event) {
 
-            int dragEvent = event.getAction();
+            int action = event.getAction();
 
-            switch (dragEvent) {
+            switch (action) {
+
+                case DragEvent.ACTION_DRAG_STARTED:
+                    View srcView = (View) event.getLocalState();
+                    if (((TextView) v).getText() == "" && (srcView.getTag() != DRAGGER_TAG || srcView.getTag() != v.getTag())) {
+                        if (event.getClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+
+                            v.setBackgroundColor(Color.YELLOW);
+
+                            return true;
+                        }
+                    }
+                    return false;
 
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    final View view = (View) event.getLocalState();
-
-                    if (view.getId() == R.id.textView) {
-                        textView3.setText("Jimmy");
-
-                    } else if (view.getId() == R.id.textView1) {
-                        textView3.setText("0405021199");
-
-                    } else if (view.getId() == R.id.textView2) {
-                        textView3.setText("August");
+                    if (((TextView) v).getText() == "") {
+                        v.setBackgroundColor(Color.GREEN);
                     }
-                    break;
 
-
+                    return true;
                 case DragEvent.ACTION_DRAG_EXITED:
+                    v.setBackgroundColor(Color.YELLOW);
 
-                    break;
+                    return true;
 
                 case DragEvent.ACTION_DROP:
-                    break;
+                    // Gets the item containing the dragged data
+                    ClipData.Item item = event.getClipData().getItemAt(0);
+
+
+                    // Makes sure text can't be dropped if there already is text.
+                    if (((TextView) v).getText() == "") {
+                        // Get dragged view object from drag event object.
+                        View view = (View) event.getLocalState();
+                        ((TextView) view).setText("");
+                        ((TextView) v).setText(item.getText());
+
+                        return true;
+
+                    }
+                case DragEvent.ACTION_DRAG_LOCATION:
+                    return true;
+                case DragEvent.ACTION_DRAG_ENDED:
+
+                    v.setBackgroundColor(Color.WHITE);
+
+
+                    // returns true; the value is ignored.
+                    return true;
+            }
+            return false;
             }
 
-            textView.animate()
+            /*textView.animate()
                     .x(textView3.getX())
                     .y(textView3.getY())
                     .setDuration(10)
-                    .start();
+                    .start();*/
 
-            return true;
-        }
     };
 
 
